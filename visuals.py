c4a4e3d66afb395e5cda6a14a936ada2dd0ef140
mehrab-haque <mehrab.haque.0001@gmail.com>
2021-07-03 07:38:04 +0600

    added documentation

--- visuals.py
+++ visuals.py
@@ -2,7 +2,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 def plot(xList,yList,func,sharpness=1000,marginPercentage=2):
-    plt.scatter(xList,yList,color="blue")
+    plt.scatter(xList,yList,color="red")
     range=max(xList)-min(xList)
     xListNew=list(np.linspace(min(xList)-range*marginPercentage/100,max(xList)+range*marginPercentage/100,sharpness))
     yListNew=[func(x) for x in xListNew]
@@ -11,5 +11,5 @@ def plot(xList,yList,func,sharpness=1000,marginPercentage=2):
     plt.axhline(y=0, color='k')
     plt.axvline(x=0, color='k')
 
-    plt.plot(xListNew, yListNew,color="green")
+    plt.plot(xListNew, yListNew)
     plt.show()
