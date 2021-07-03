e95d23884d9de7ee5b4df410b00e859bb520f7b7
mehrab-haque <mehrab.haque.0001@gmail.com>
2021-07-03 06:03:37 +0600

    linear regression completed

--- linear_regression.py
+++ linear_regression.py
@@ -1,4 +1,4 @@
-def linearRegression(xList,yList):
+def linearRegression(xList,yList,origin=False):
     n=len(xList)
     sumXY=sum(xList[i]*yList[i] for i in range(len(xList)))
     sumX=sum(xList)
@@ -6,4 +6,7 @@ def linearRegression(xList,yList):
     sumXX=sum(i*i for i in xList)
     a1=(n*sumXY-sumX*sumY)/(n*sumXX-sumX*sumX)
     a0=(sumY-a1*sumX)/n
+    if origin:
+        a0=0
+        a1=sumXY/sumXX
     return a0,a1
